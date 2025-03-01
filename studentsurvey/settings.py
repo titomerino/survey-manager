a9f66ee8b1db26c169781d711661f5218e3168e4
Tito Merino <tiaguilar@stefanini.com>
2025-03-01 00:02:09 -0600

    Add csrf

--- studentsurvey/settings.py
+++ studentsurvey/settings.py
@@ -137,3 +137,6 @@ STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+
+CSRF_TRUSTED_ORIGINS = ['http://*', 'https://survey-manager-production.up.railway.app']
